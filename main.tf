8aec7b72bc238f4171ae91928805fdce0f581038
Tao He <tao.he1@dell.com>
2023-11-30 05:28:44 +0000

    support powerscale and powermax provider together

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
 provider "powerscale" {
-  username = "root"
-  password = "Password123!"
-  endpoint = "https://10.225.108.6:8080"
-  insecure = true
+  username = var.username
+  password = var.password
+  endpoint = var.endpoint
+  insecure = var.insecure
+}
+
+provider "powermax" {
+  username      = var.powermax_username
+  password      = var.powermax_password
+  endpoint      = var.powermax_endpoint
+  serial_number = var.powermax_serial_number
+  pmax_version  = var.pmax_version
+  insecure = var.powermax_insecure
 }
 
 resource "powerscale_user" "testUser" {
@@ -25,7 +34,7 @@ resource "powerscale_user" "testUser" {
   # uid      = 11000
   # password = "testPassword"
   roles    = ["SystemAdmin"]
-  # enabled = false
+  enabled = true
   # unlock = false
   # email = "testTerraform@dell.com"
   # home_directory = "/ifs/home/testUserResourceSample"
@@ -40,4 +49,26 @@ resource "powerscale_user" "testUser" {
 resource "powerscale_user_group" "testUserGroup" {
   # Required name for creating
   name = "testUserGroupCloud"
+}
+
+resource "powermax_volume" "testVolume" {
+
+  # Attributes which are able to be modified after create (vol_name, mobility_id_enabled, size, cap_unit)
+
+  # Required name of the volume to be created
+  vol_name = "terraform_cloud_volume"
+
+  # Required size of the volume
+  size = 2.45
+
+  # Required name of the storage group which the volume will be created with
+  sg_name = "tfacc_cloud_volume"
+
+  # Optional Default Unit is GB
+  # Possible units are MB, GB, TB, and CYL
+  cap_unit = "GB"
+
+  # Optional enable the mobility id 
+  mobility_id_enabled = false
+
 }
